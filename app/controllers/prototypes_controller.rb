13e172e0df681e7821a0931ea0b2a3ae5099fade
takuya20201003 <takuya@MacBook-Air.local>
2021-02-05 17:58:03 +0900

    指摘の修正１

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -44,16 +44,17 @@ class PrototypesController < ApplicationController
     redirect_to root_path
   end
 
-end
 
-private
 
-def prototype_params
-  params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
-end
+  private
 
-def move_to_index
-  unless user_signed_in?
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
     redirect_to action: :index
+    end
   end
 end
